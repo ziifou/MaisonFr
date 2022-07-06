c10f47c5bcf0530c2cb87ed1e6b838bc64236c13
ziifou <sifeddine228@gmail.com>
2022-07-06 21:27:33 +0200

    correction

--- src/components/CreateListing.js
+++ src/components/CreateListing.js
@@ -90,7 +90,7 @@ function CreateListing() {
 
     if (geolocationEnabled) {
       const response = await fetch(
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=dsdsdsds`
       );
       const data = await response.json();
 
